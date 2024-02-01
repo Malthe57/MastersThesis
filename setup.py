a222331708f07c6343237b9dcd18390096565db2
Malthe Andersen <s194257@student.dtu.dk>
2024-02-01 10:37:45 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Uncertainty quantification with subnetwork ensemble methods for neural networks',
+    author='Yucheng Fu & Malthe Dohm Andersen',
+    license='MIT',
+)
