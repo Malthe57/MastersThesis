Corrected sigma computation in varMIMO

src/models/mimo.py
@@ -60,7 +60,7 @@ class VarMIMONetwork(nn.Module):
         # compute mu and sigma for mixture model with M gaussian
         # https://stats.stackexchange.com/a/445232
         mu = torch.mean(mus, dim=1)
-        sigma = torch.mean((mus.pow(2) + sigmas.pow(2)), dim=1).sqrt()
+        sigma = (torch.mean((mus.pow(2) + sigmas.pow(2)), dim=1) - mu.pow(2)).sqrt()
         
         return mu, sigma, mus, sigmas
     

src/train_regression.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 from torch.utils.data import DataLoader 
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-from visualization.visualize import plot_loss, plot_log_probs, 
+from visualization.visualize import plot_loss, plot_log_probs
 from models.mimo import MIMONetwork, NaiveNetwork, C_MIMONetwork, C_NaiveNetwork, VarMIMONetwork
 from models.bnn import BayesianNeuralNetwork, BayesianConvNeuralNetwork
 from utils.utils import seed_worker, set_seed, init_weights