9493505c44d8a9b634ff739756af702ab92efac3
Malthe Andersen <s194257@student.dtu.dk>
2024-03-14 12:35:34 +0100

    Create jobscript_classification.sh

---
+++ jobscript_classification.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#BSUB -J Regression
+#BSUB -o Regression%J.out
+#BSUB -e Regression%J.err
+#BSUB -q gpuv100
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -n 1
+#BSUB -R "rusage[mem=8G]"
+#BSUB -W 4:00
+#BSUB -N
+# end of BSUB options
+
+module load python3/3.11.7
+
+# load CUDA (for GPU support)
+module load cuda/11.8
+
+# activate the virtual environment
+source MT/bin/activate
+
+python src/train_classification.py experiments=train_classification_baseline
+python src/train_classification.py experiments=train_classification_bnn
