acd64d12d3e95c178e58fe79de5450e7c48d4b3a
Malthe Andersen <s194257@student.dtu.dk>
2024-03-14 11:10:15 +0100

    Update jobscript_classification.sh

--- jobscript_classification.sh
+++ jobscript_classification.sh
@@ -18,4 +18,5 @@ module load cuda/11.8
 # activate the virtual environment
 source MT/bin/activate
 
-python src/train_classification.py experiments=train_classification_mimbo
+python src/train_classification.py experiments=train_classification_baseline
+python src/train_classification.py experiments=train_classification_bnn
