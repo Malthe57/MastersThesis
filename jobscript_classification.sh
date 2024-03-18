232c2f0fb91f86a4599e4f1bbce1b267bd6d1252
Malthe Andersen <s194257@student.dtu.dk>
2024-03-18 08:37:56 +0100

    added variance calculation notebook

--- jobscript_classification.sh
+++ jobscript_classification.sh
@@ -19,3 +19,4 @@ module load cuda/11.8
 source MT/bin/activate
 
 python src/train_classification.py experiments=train_classification_baseline
+python src/train_classification.py experiments=train_classification_bnn
