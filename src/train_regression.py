ce2945dca87b1604e2c01ab77a9926037b7c983c
Malthe Andersen <s194257@student.dtu.dk>
2024-04-10 09:13:46 +0200

    Update train_regression.py

--- src/train_regression.py
+++ src/train_regression.py
@@ -74,7 +74,7 @@ def main_mimo(cfg: dict, rep : int, seed : int) -> None:
         traindata, valdata, _, input_dim, _, _, _ = load_multireg_data(dataset)
     
     elif dataset=='crimedata':
-        prepare_crime(overwrite=True)
+        prepare_crime(overwrite=False)
         traindata, valdata, _, input_dim, _, _, _ = load_multireg_data(dataset)
  
     if naive == False:
