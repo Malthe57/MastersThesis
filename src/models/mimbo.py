423c44f72820dfcd27ca0be2b59555f233142ded
TECH-yufu <yuch0001@gmail.com>
2024-04-18 13:07:29 +0200

    Enabling posterior

--- src/models/mimbo.py
+++ src/models/mimbo.py
@@ -175,9 +175,9 @@ class MIMBOConvNeuralNetwork(nn.Module):
 
     def compute_log_variational_posterior(self):
         model_log_variational_posterior = 0.0
-        # for layer in self.layers:
-        #     if isinstance(layer, BayesianLinearLayer) or isinstance(layer, BayesianConvLayer):
-        #         model_log_variational_posterior += layer.log_variational_posterior
+        for layer in self.layers:
+            if isinstance(layer, BayesianLinearLayer) or isinstance(layer, BayesianConvLayer):
+                model_log_variational_posterior += layer.log_variational_posterior
         return model_log_variational_posterior
     
     def compute_NLL(self, log_probs, target, val=False):
