22dced9c943620bf64708a559b2bfc31d9fddf9c
TECH-yufu <yuch0001@gmail.com>
2024-04-18 13:33:35 +0200

    Update mimbo.py

--- src/models/mimbo.py
+++ src/models/mimbo.py
@@ -309,15 +309,15 @@ class MIMBOWideResnet(nn.Module):
     
     def compute_log_variational_posterior(self):
         model_log_variational_posterior = 0.0
-        # for layer in [self.layer1, self.layer2, self.layer3, self.layer4, self.linear]:
-        #     # layer can either be BayesianConvLayer or nn.Sequential() containing 4 Bayesian Blocks
-        #     if isinstance(layer, BayesianLinearLayer) or isinstance(layer, BayesianConvLayer):
-        #         model_log_variational_posterior += layer.log_variational_posterior
-        #     elif isinstance(layer, nn.Sequential):
-        #         for block in layer:
-        #             for module in block.modules():
-        #                 if isinstance(module, BayesianLinearLayer) or isinstance(module, BayesianConvLayer):
-        #                     model_log_variational_posterior += module.log_variational_posterior 
+        for layer in [self.layer1, self.layer2, self.layer3, self.layer4, self.linear]:
+            # layer can either be BayesianConvLayer or nn.Sequential() containing 4 Bayesian Blocks
+            if isinstance(layer, BayesianLinearLayer) or isinstance(layer, BayesianConvLayer):
+                model_log_variational_posterior += layer.log_variational_posterior
+            elif isinstance(layer, nn.Sequential):
+                for block in layer:
+                    for module in block.modules():
+                        if isinstance(module, BayesianLinearLayer) or isinstance(module, BayesianConvLayer):
+                            model_log_variational_posterior += module.log_variational_posterior 
         return model_log_variational_posterior
     
     def compute_NLL(self, log_probs, target, val=False):
