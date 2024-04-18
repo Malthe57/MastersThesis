ffc457976764c846366efb0c7b4135355ac2b116
TECH-yufu <yuch0001@gmail.com>
2024-04-18 09:17:53 +0200

    turning off prior and posterior calculation

--- src/models/mimbo.py
+++ src/models/mimbo.py
@@ -296,28 +296,28 @@ class MIMBOWideResnet(nn.Module):
     
     def compute_log_prior(self):
         model_log_prior = 0.0
-        for layer in [self.layer1, self.layer2, self.layer3, self.layer4, self.linear]:
-            # layer can either be BayesianConvLayer or nn.Sequential() containing 4 Bayesian Blocks
-            if isinstance(layer, BayesianLinearLayer) or isinstance(layer, BayesianConvLayer):
-                model_log_prior += layer.log_prior
-            elif isinstance(layer, nn.Sequential):
-                for block in layer:
-                    for module in block.modules():
-                        if isinstance(module, BayesianLinearLayer) or isinstance(module, BayesianConvLayer):
-                            model_log_prior += module.log_prior
+        # for layer in [self.layer1, self.layer2, self.layer3, self.layer4, self.linear]:
+        #     # layer can either be BayesianConvLayer or nn.Sequential() containing 4 Bayesian Blocks
+        #     if isinstance(layer, BayesianLinearLayer) or isinstance(layer, BayesianConvLayer):
+        #         model_log_prior += layer.log_prior
+        #     elif isinstance(layer, nn.Sequential):
+        #         for block in layer:
+        #             for module in block.modules():
+        #                 if isinstance(module, BayesianLinearLayer) or isinstance(module, BayesianConvLayer):
+        #                     model_log_prior += module.log_prior
         return model_log_prior
     
     def compute_log_variational_posterior(self):
         model_log_variational_posterior = 0.0
-        for layer in [self.layer1, self.layer2, self.layer3, self.layer4, self.linear]:
-            # layer can either be BayesianConvLayer or nn.Sequential() containing 4 Bayesian Blocks
-            if isinstance(layer, BayesianLinearLayer) or isinstance(layer, BayesianConvLayer):
-                model_log_variational_posterior += layer.log_variational_posterior
-            elif isinstance(layer, nn.Sequential):
-                for block in layer:
-                    for module in block.modules():
-                        if isinstance(module, BayesianLinearLayer) or isinstance(module, BayesianConvLayer):
-                            model_log_variational_posterior += module.log_variational_posterior 
+        # for layer in [self.layer1, self.layer2, self.layer3, self.layer4, self.linear]:
+        #     # layer can either be BayesianConvLayer or nn.Sequential() containing 4 Bayesian Blocks
+        #     if isinstance(layer, BayesianLinearLayer) or isinstance(layer, BayesianConvLayer):
+        #         model_log_variational_posterior += layer.log_variational_posterior
+        #     elif isinstance(layer, nn.Sequential):
+        #         for block in layer:
+        #             for module in block.modules():
+        #                 if isinstance(module, BayesianLinearLayer) or isinstance(module, BayesianConvLayer):
+        #                     model_log_variational_posterior += module.log_variational_posterior 
         return model_log_variational_posterior
     
     def compute_NLL(self, log_probs, target, val=False):
