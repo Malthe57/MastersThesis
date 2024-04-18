d838cd2ec0e30e8a9b78b2f788d7c64a16cff03d
TECH-yufu <yuch0001@gmail.com>
2024-04-18 10:10:06 +0200

    Update mimbo.py

--- src/models/mimbo.py
+++ src/models/mimbo.py
@@ -168,16 +168,16 @@ class MIMBOConvNeuralNetwork(nn.Module):
 
     def compute_log_prior(self):
         model_log_prior = 0.0
-        for layer in self.layers:
-            if isinstance(layer, BayesianLinearLayer) or isinstance(layer, BayesianConvLayer):
-                model_log_prior += layer.log_prior
+        # for layer in self.layers:
+        #     if isinstance(layer, BayesianLinearLayer) or isinstance(layer, BayesianConvLayer):
+        #         model_log_prior += layer.log_prior
         return model_log_prior
 
     def compute_log_variational_posterior(self):
         model_log_variational_posterior = 0.0
-        for layer in self.layers:
-            if isinstance(layer, BayesianLinearLayer) or isinstance(layer, BayesianConvLayer):
-                model_log_variational_posterior += layer.log_variational_posterior
+        # for layer in self.layers:
+        #     if isinstance(layer, BayesianLinearLayer) or isinstance(layer, BayesianConvLayer):
+        #         model_log_variational_posterior += layer.log_variational_posterior
         return model_log_variational_posterior
     
     def compute_NLL(self, log_probs, target, val=False):
