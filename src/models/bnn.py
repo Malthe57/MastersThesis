4440e576806a99271fe33276c5e4f852bc790736
TECH-yufu <yuch0001@gmail.com>
2024-04-25 09:00:48 +0200

    #41

--- src/models/bnn.py
+++ src/models/bnn.py
@@ -504,12 +504,12 @@ class BayesianWideResnet(nn.Module):
         for layer in [self.layer1, self.layer2, self.layer3, self.layer4, self.linear]:
             # layer can either be BayesianConvLayer or nn.Sequential() containing 4 Bayesian Blocks
             if isinstance(layer, BayesianLinearLayer) or isinstance(layer, BayesianConvLayer):
-                model_log_prior += layer.log_prior
+                model_log_prior += layer.log_prior*0
             elif isinstance(layer, nn.Sequential):
                 for block in layer:
                     for module in block.modules():
                         if isinstance(module, BayesianLinearLayer) or isinstance(module, BayesianConvLayer):
-                            model_log_prior += module.log_prior
+                            model_log_prior += module.log_prior*0
         return model_log_prior
     
     def compute_log_variational_posterior(self):
