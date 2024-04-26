06026557cef5c7fb82a1d041fa50a6e0f68b942c
TECH-yufu <yuch0001@gmail.com>
2024-04-26 05:33:19 +0200

    leaky relu :3

--- src/models/bnn.py
+++ src/models/bnn.py
@@ -240,7 +240,7 @@ class BayesianConvLayer(nn.Module):
         
         # initialise mu and rho parameters so they get updated in backpropagation
         # use *kernel_size instead of writing (_, _, kernel_size, kernel_size)
-        self.weight_rho = nn.Parameter(torch.Tensor(out_channels, in_channels, *kernel_size).normal_(-5,0.1))
+        self.weight_rho = nn.Parameter(torch.Tensor(out_channels, in_channels, *kernel_size).uniform_(-6,-5))
         self.weight_mu = nn.Parameter(torch.Tensor(out_channels, in_channels, *kernel_size))
         # self.bias_rho = nn.Parameter(torch.Tensor(out_channels).uniform_(-6, -5))
         # self.bias_mu = nn.Parameter(torch.Tensor(out_channels))
@@ -425,8 +425,8 @@ class BayesianWideBlock(nn.Module):
             )
 
     def forward (self, x):
-        out = self.dropout(self.conv1(F.relu(self.bn1(x))))
-        out = self.conv2(F.relu(self.bn2(out)))
+        out = self.dropout(self.conv1(F.leaky_relu(self.bn1(x), negative_slope=0.02)))
+        out = self.conv2(F.leaky_relu(self.bn2(out), negative_slope=0.02))
         out += self.skip(x)
 
         return out
@@ -475,7 +475,7 @@ class BayesianWideResnet(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = F.relu(self.bn1(out))
+        out = F.leaky_relu(self.bn1(out), negative_slope=0.02)
         out = F.avg_pool2d(out, 8)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
