6148dbddc1e6bb18a30e8cab58089445d48b8da2
TECH-yufu <yuch0001@gmail.com>
2024-04-30 14:39:20 +0200

    Implementing NLL in inference for classificaition

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -27,6 +27,18 @@ def compute_brier_score(model_probabilities, correct_class):
 
     return np.mean(brier_score, axis=0)
 
+def compute_NLL(log_probs, targets):
+    """
+    This function computes the negative log likelihood of a model.
+    log_probs is assumed to be in the shape (N_test, c), where C is the amount of classes, while N_test is the number of test points
+    The targets input contains information on the ground truth class (N_test)
+    """
+    loss_fn = torch.nn.NLLLoss()
+    NLL = loss_fn(torch.tensor(log_probs), torch.tensor(targets))
+
+    return NLL.item()
+
+
 if __name__ == "__main__":
     model_probabilities = np.array([[0.78, 0.08, 0.14], [0.05, 0.81, 0.14]])
     correct_class = np.array([0,1])
