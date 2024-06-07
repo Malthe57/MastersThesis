770d7d34b97ef2b71eb7482ac2acaee8e0d16d8f
TECH-yufu <yuch0001@gmail.com>
2024-06-07 11:30:14 +0200

    Update visualize_classification.py

--- src/visualize_classification.py
+++ src/visualize_classification.py
@@ -36,54 +36,56 @@ def model_accuracy(correct_preds_matrix : torch.tensor):
 
     return per_rep_accuracy, per_rep_SE
 
-if __name__ == '__main__':
+def accuracy_and_ECE():
+    dataset = "CIFAR100_C"
 
-    dataset = "CIFAR10"
+    models = ["C_MIMOWide"]
 
-    models = ["C_MIMBO"]
+    severity = 5
 
-    severity = None
+    Ms = [1,2,3,4,5]
 
-    Ms = [2,3,4,5]
+    for model in models:
+        print("Visualizing model:", model)
+        try:
+            NPZ = np.load(f"reports/Logs/{model}/{dataset}/{model}_severity{severity}.npz") if severity else np.load(f"reports/Logs/{model}/{dataset}/{model}.npz")
+        except:
+            print(f"No {model} model found!")
+        else:
+            predictions, confidences, full_confidences, correct_preds, targets, brier_scores, NLLs = NPZ["predictions"], NPZ["confidences"], NPZ["full_confidences"], NPZ["correct_preds"], NPZ["targets_matrix"], NPZ["brier_score"], NPZ["NLL"]
+            for i, M in enumerate(Ms):
+                if "BNN" in model:
+                    per_rep_accuracy, per_rep_SE = model_accuracy(correct_preds)   
+                    reliability_plot_classification_single(correct_predictions=correct_preds, confidence=confidences, model_name=model, dataset=dataset, severity=severity)
+                    print(f"{model} test accuracy: {per_rep_accuracy} \pm {1.96*per_rep_SE} \n")
+                else:
+                    per_rep_accuracy, per_rep_SE = model_accuracy(correct_preds[:,:,i])   
+                    reliability_plot_classification_single(correct_predictions=correct_preds[:,:,i], confidence=confidences[:,:,i], model_name='C_Baseline' if M == 1 else model, dataset=dataset, M=M, severity=severity)
+                    print(f"{model} M{M} test accuracy: {per_rep_accuracy} \pm {1.96*per_rep_SE} \n")
 
-    # for model in models:
-    #     print("Visualizing model:", model)
-    #     try:
-    #         NPZ = np.load(f"reports/Logs/{model}/{dataset}/{model}_severity{severity}.npz") if severity else np.load(f"reports/Logs/{model}/{dataset}/{model}.npz")
-    #     except:
-    #         print(f"No {model} model found!")
-    #     else:
-    #         predictions, confidences, full_confidences, correct_preds, targets, brier_scores, NLLs = NPZ["predictions"], NPZ["confidences"], NPZ["full_confidences"], NPZ["correct_preds"], NPZ["targets_matrix"], NPZ["brier_score"], NPZ["NLL"]
-    #         for M in Ms:
-    #             if "BNN" in model:
-    #                 per_rep_accuracy, per_rep_SE = model_accuracy(correct_preds)   
-    #                 reliability_plot_classification_single(correct_predictions=correct_preds, confidence=confidences, model_name=model, dataset=dataset, severity=severity)
-    #                 print(f"{model} test accuracy: {per_rep_accuracy} \pm {1.96*per_rep_SE} \n")
-    #             else:
-    #                 per_rep_accuracy, per_rep_SE = model_accuracy(correct_preds[:,:,M-2])   
-    #                 reliability_plot_classification_single(correct_predictions=correct_preds[:,:,M-2], confidence=confidences[:,:,M-2], model_name='C_Baseline' if M == 1 else model, dataset=dataset, M=M, severity=severity)
-    #                 print(f"{model} M{M} test accuracy: {per_rep_accuracy} \pm {1.96*per_rep_SE} \n")
+def function_space():
+    try:
+        Ms = [3]
+        dataset = "CIFAR10"
+        for M in Ms:
+            checkpoint_list = []
+            checkpoint_list.append(torch.load(f'models/classification/checkpoints/C_MIMOWide/{dataset}/M{M}/C_MIMOWide_28_10_{M}_members_rep1_checkpoints.pt', map_location=torch.device('cpu'))[:,:,:,:])
+            checkpoint_list.append(torch.load(f'models/classification/checkpoints/C_NaiveWide/{dataset}/M{M}/C_NaiveWide_28_10_{M}_members_rep1_checkpoints.pt', map_location=torch.device('cpu'))[:,:,:,:])
+            checkpoint_list.append(torch.load(f'models/classification/checkpoints/C_MIMBOWide/{dataset}/M{M}/C_MIMBOWide_28_10_{M}_members_rep1_checkpoints.pt', map_location=torch.device('cpu'))[:,:,:,:])
+    except:
+        print('Try again loser >:)')
+    else:
+        multi_function_space_plots(checkpoint_list, ['C_MIMO','C_Naive','C_MIMBO'], n_samples=5, perplexity=15, num_components=3, algorithm='PCA')
     
-    
-    # try:
-    #     Ms = [3]
-    #     dataset = "CIFAR10"
-    #     for M in Ms:
-    #         checkpoint_list = []
-    #         checkpoint_list.append(torch.load(f'models/classification/checkpoints/C_MIMOWide/{dataset}/M{M}/C_MIMOWide_28_10_{M}_members_rep1_checkpoints.pt', map_location=torch.device('cpu'))[:,:,:,:])
-    #         checkpoint_list.append(torch.load(f'models/classification/checkpoints/C_NaiveWide/{dataset}/M{M}/C_NaiveWide_28_10_{M}_members_rep1_checkpoints.pt', map_location=torch.device('cpu'))[:,:,:,:])
-    #         checkpoint_list.append(torch.load(f'models/classification/checkpoints/C_MIMBOWide/{dataset}/M{M}/C_MIMBOWide_28_10_{M}_members_rep1_checkpoints.pt', map_location=torch.device('cpu'))[:,:,:,:])
-    # except:
-    #     print('Try again loser >:)')
-    # else:
-    #     multi_function_space_plots(checkpoint_list, ['C_MIMO','C_Naive','C_MIMBO'], n_samples=5, perplexity=15, num_components=3, algorithm='PCA')
-
-
+def plot_prediction_example():
     Ms = [3]
     dataset = "CIFAR10"
     # plot_prediction_example(4, architectures=['MediumCNN','WideResnet'], models=['MIMO','MIMBO'], M=4, dataset=dataset, severity=5)
     data_space_plot(dataset=dataset, severity=1)
 
+if __name__ == '__main__':
+
+    accuracy_and_ECE()
 
 
 
