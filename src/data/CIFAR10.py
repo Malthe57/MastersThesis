b95066d88460c92133698d37519ffaf1f4e806bf
TECH-yufu <yuch0001@gmail.com>
2024-07-22 16:38:37 +0200

    Batch repetition + augmentation experiment

--- src/data/CIFAR10.py
+++ src/data/CIFAR10.py
@@ -9,56 +9,55 @@ import numpy as np
 
 def load_cifar10(data_path: str, data_aug = True):
     # normalisation values from https://github.com/kuangliu/pytorch-cifar/issues/19
-    transform  = transforms.Compose(
-        [transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))])
-
-    generator = torch.Generator().manual_seed(1871)
-    CIFAR_traindata = torchvision.datasets.CIFAR10(root=data_path, train=True, transform = transform, download=True)
-    CIFAR_train, CIFAR_val = torch.utils.data.random_split(CIFAR_traindata, [int(len(CIFAR_traindata)*0.9), int(len(CIFAR_traindata)*0.1)], generator=generator)
-
-    CIFAR_test = torchvision.datasets.CIFAR10(root=data_path, train=False, transform = transform, download=True)
-    return CIFAR_train, CIFAR_val, CIFAR_test
-    
-    
-    # normalize = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))
-    
-    # if data_aug == True:
-    #     #Transforms from https://github.com/xternalz/WideResNet-pytorch/blob/master/train.py
-    #     transform_train = transforms.Compose(
+    # transform  = transforms.Compose(
     #     [transforms.ToTensor(),
-    #     transforms.Lambda(lambda x: F.pad(x.unsqueeze(0),
-    #     						(4,4,4,4),mode='reflect').squeeze()),
-    #     transforms.ToPILImage(),
-    #     transforms.RandomCrop(32),
-    #     transforms.RandomHorizontalFlip(),
-    #     transforms.ToTensor(),
-    #     normalize]
-    # )
-    # else:
-    #     transform_train = transforms.Compose(
-    #     [transforms.ToTensor(),
-    #      normalize])
+    #         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))])
 
-    # transform_test  = transforms.Compose(
-    #     [transforms.ToTensor(),
-    #      normalize])
-    
     # generator = torch.Generator().manual_seed(1871)
-    # CIFAR_traindata = torchvision.datasets.CIFAR10(root=data_path, train=True, transform=transforms.ToTensor(), download=True)
-    # len_train = int(len(CIFAR_traindata)*0.9)
-    # len_val = int(len(CIFAR_traindata)*0.1)
-    # randperm = torch.randperm(50000, generator=generator)
+    # CIFAR_traindata = torchvision.datasets.CIFAR10(root=data_path, train=True, transform = transform, download=True)
+    # CIFAR_train, CIFAR_val = torch.utils.data.random_split(CIFAR_traindata, [int(len(CIFAR_traindata)*0.9), int(len(CIFAR_traindata)*0.1)], generator=generator)
 
-    # CIFAR_train_x = CIFAR_traindata.data[randperm[:len_train]]
-    # CIFAR_train_y = np.array(CIFAR_traindata.targets)[randperm[:len_train]]
-    # CIFAR_val_x = CIFAR_traindata.data[randperm[len_train:]]
-    # CIFAR_val_y = np.array(CIFAR_traindata.targets)[randperm[len_train:]]
-    # CIFAR_train = CIFAR10(CIFAR_train_x, CIFAR_train_y, transform_train)
-    # CIFAR_val = CIFAR10(CIFAR_val_x, CIFAR_val_y, transform_test)
-
-    # CIFAR_test = torchvision.datasets.CIFAR10(root=data_path, train=False, transform = transform_test, download=True)
+    # CIFAR_test = torchvision.datasets.CIFAR10(root=data_path, train=False, transform = transform, download=True)
     # return CIFAR_train, CIFAR_val, CIFAR_test
+    
+    
+    normalize = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))
+    
+    if data_aug == True:
+        # Transforms from https://github.com/xternalz/WideResNet-pytorch/blob/master/train.py
+        transform_train = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Lambda(lambda x: F.pad(x.unsqueeze(0), (4,4,4,4),mode='reflect').squeeze()),
+        transforms.ToPILImage(),
+        transforms.RandomCrop(32),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        normalize]
+    )
+    else:
+        transform_train = transforms.Compose(
+        [transforms.ToTensor(),
+         normalize])
+
+    transform_test  = transforms.Compose(
+        [transforms.ToTensor(),
+         normalize])
+    
+    generator = torch.Generator().manual_seed(1871)
+    CIFAR_traindata = torchvision.datasets.CIFAR10(root=data_path, train=True, transform=transforms.ToTensor(), download=True)
+    len_train = int(len(CIFAR_traindata)*0.9)
+    len_val = int(len(CIFAR_traindata)*0.1)
+    randperm = torch.randperm(50000, generator=generator)
+
+    CIFAR_train_x = CIFAR_traindata.data[randperm[:len_train]]
+    CIFAR_train_y = np.array(CIFAR_traindata.targets)[randperm[:len_train]]
+    CIFAR_val_x = CIFAR_traindata.data[randperm[len_train:]]
+    CIFAR_val_y = np.array(CIFAR_traindata.targets)[randperm[len_train:]]
+    CIFAR_train = CIFAR10(CIFAR_train_x, CIFAR_train_y, transform_train)
+    CIFAR_val = CIFAR10(CIFAR_val_x, CIFAR_val_y, transform_test)
+
+    CIFAR_test = torchvision.datasets.CIFAR10(root=data_path, train=False, transform = transform_test, download=True)
+    return CIFAR_train, CIFAR_val, CIFAR_test
 
 class CIFAR10(Dataset):
     def __init__(self, data, targets, transform):
@@ -124,13 +123,17 @@ def C_train_collate_fn(batch, M, batch_repetition=1):
     x, y = zip(*batch)
 
     x, y = torch.stack(list(x)), torch.tensor(y)
-    x = torch.cat(torch.chunk(x, M, dim=0), dim=1)
-    y = torch.stack(torch.chunk(y, M, dim=0), dim=1)
 
     if batch_repetition > 1:
         x = x.repeat(batch_repetition, 1, 1, 1)
-        y = y.repeat(batch_repetition, 1)
+        y = y.repeat(batch_repetition)
+    
+        indices = torch.randperm(x.size(0))
+        x = x[indices]
+        y = y[indices]
 
+    x = torch.cat(torch.chunk(x, M, dim=0), dim=1)
+    y = torch.stack(torch.chunk(y, M, dim=0), dim=1)
     return x, y
 
 def C_test_collate_fn(batch, M):
