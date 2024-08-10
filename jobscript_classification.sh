05161d7df1a34d65442564564aeba15a5980422b
Yucheng Fu <80991181+TECH-yufu@users.noreply.github.com>
2024-08-10 16:50:26 +0200

    Add files via upload

---
+++ jobscript_classification.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#BSUB -J Classification
+#BSUB -o Classification%J.out
+#BSUB -e Classification%J.err
+#BSUB -q gpuv100
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -n 4
+#BSUB -R "rusage[mem=8G]"
+#BSUB -W 6:00
+#BSUB -N
+# end of BSUB options
+
+module load python3/3.11.7
+
+# load CUDA (for GPU support)
+module load cuda/11.8
+
+# activate the virtual environment
+source MT/bin/activate
+
+python src/train_classification.py experiments=train_classification_MIMO experiments.n_subnetworks=2
+python src/train_classification.py experiments=train_classification_MIMO experiments.n_subnetworks=3
+python src/train_classification.py experiments=train_classification_MIMO experiments.n_subnetworks=4
+python src/train_classification.py experiments=train_classification_MIMO experiments.n_subnetworks=5
+
+python src/train_classification.py experiments=train_classification_naive experiments.n_subnetworks=2
+python src/train_classification.py experiments=train_classification_naive experiments.n_subnetworks=3
+python src/train_classification.py experiments=train_classification_naive experiments.n_subnetworks=4
+python src/train_classification.py experiments=train_classification_naive experiments.n_subnetworks=5
